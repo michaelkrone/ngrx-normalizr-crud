7256df3ecd285542dfa99672322dae42927c6de5
Xue Qin <xq@gastro-instruments.de>
2018-02-20 13:38:36 +0100

    chore: correct indent style

--- src/actions/create.actions.ts
+++ src/actions/create.actions.ts
@@ -5,60 +5,60 @@ import { PayloadAction } from '../classes/payload-action';
  * Interface for create action names
  */
 export interface EntityCreateActionNames {
-  /**
-   * Defining the type of the `CREATE` action type
-   */
-  CREATE: string;
+	/**
+	 * Defining the type of the `CREATE` action type
+	 */
+	CREATE: string;
 
-  /**
-   * Defining the type of the `CREATE_SUCCESS` action type
-   */
-  CREATE_SUCCESS: string;
+	/**
+	 * Defining the type of the `CREATE_SUCCESS` action type
+	 */
+	CREATE_SUCCESS: string;
 
-  /**
-   * Defining the type of the `CREATE_FAIL` action type
-   */
-  CREATE_FAIL: string;
+	/**
+	 * Defining the type of the `CREATE_FAIL` action type
+	 */
+	CREATE_FAIL: string;
 }
 
 /**
  * Interface for `createCreateActions` return type
  */
 export interface CreateActions<T> extends EntityCreateActionNames {
-  /**
-   * Type of the `Create` action
-   */
-  Create: new (payload: T) => PayloadAction<T>;
+	/**
+	 * Type of the `Create` action
+	 */
+	Create: new (payload: T) => PayloadAction<T>;
 
-  /**
-   * Type of the `CreateSuccess` action
-   */
-  CreateSuccess: new (payload: T) => PayloadAction<T>;
+	/**
+	 * Type of the `CreateSuccess` action
+	 */
+	CreateSuccess: new (payload: T) => PayloadAction<T>;
 
-  /**
-   * Type of the `CreateFail` action
-   */
-  CreateFail: new (payload?: any) => PayloadAction<any>;
+	/**
+	 * Type of the `CreateFail` action
+	 */
+	CreateFail: new (payload?: any) => PayloadAction<any>;
 }
 
 /**
  * Action name constants
  */
 export const CreateActionNames: EntityCreateActionNames = {
-  /**
-   * Value of the `CREATE` action type
-   */
-  CREATE: 'Create',
+	/**
+	 * Value of the `CREATE` action type
+	 */
+	CREATE: 'Create',
 
-  /**
-   * Value of the `CREATE_SUCCESS` action type
-   */
-  CREATE_SUCCESS: 'Create Success',
+	/**
+	 * Value of the `CREATE_SUCCESS` action type
+	 */
+	CREATE_SUCCESS: 'Create Success',
 
-  /**
-   * Value of the `CREATE_FAIL` action type
-   */
-  CREATE_FAIL: 'Create Fail'
+	/**
+	 * Value of the `CREATE_FAIL` action type
+	 */
+	CREATE_FAIL: 'Create Fail'
 };
 
 /**
@@ -68,37 +68,37 @@ export const CreateActionNames: EntityCreateActionNames = {
  * @param ns The namespace to use for creating action type constants
  */
 export function createCreateActions<T>(ns: string): CreateActions<T> {
-  const CREATE = ActionNameFactory.getActionName(CreateActionNames.CREATE, ns);
-  const CREATE_SUCCESS = ActionNameFactory.getActionName(
-    CreateActionNames.CREATE_SUCCESS,
-    ns
-  );
-  const CREATE_FAIL = ActionNameFactory.getActionName(
-    CreateActionNames.CREATE_FAIL,
-    ns
-  );
+	const CREATE = ActionNameFactory.getActionName(CreateActionNames.CREATE, ns);
+	const CREATE_SUCCESS = ActionNameFactory.getActionName(
+		CreateActionNames.CREATE_SUCCESS,
+		ns
+	);
+	const CREATE_FAIL = ActionNameFactory.getActionName(
+		CreateActionNames.CREATE_FAIL,
+		ns
+	);
 
-  class Create implements PayloadAction<T> {
-    readonly type = CREATE;
-    constructor(public payload: T) {}
-  }
+	class Create implements PayloadAction<T> {
+		readonly type = CREATE;
+		constructor(public payload: T) {}
+	}
 
-  class CreateSuccess implements PayloadAction<T> {
-    readonly type = CREATE_SUCCESS;
-    constructor(public payload: T) {}
-  }
+	class CreateSuccess implements PayloadAction<T> {
+		readonly type = CREATE_SUCCESS;
+		constructor(public payload: T) {}
+	}
 
-  class CreateFail implements PayloadAction<any> {
-    readonly type = CREATE_FAIL;
-    constructor(public payload: any | undefined) {}
-  }
+	class CreateFail implements PayloadAction<any> {
+		readonly type = CREATE_FAIL;
+		constructor(public payload: any | undefined) {}
+	}
 
-  return {
-    Create,
-    CREATE,
-    CreateSuccess,
-    CREATE_SUCCESS,
-    CreateFail,
-    CREATE_FAIL
-  };
+	return {
+		Create,
+		CREATE,
+		CreateSuccess,
+		CREATE_SUCCESS,
+		CreateFail,
+		CREATE_FAIL
+	};
 }
